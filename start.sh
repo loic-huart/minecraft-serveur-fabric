86f7b7182de9ef40b2a635f1dc4d80fd493ef8a6
loic.huart <loic.huart08@gmail.com>
2023-07-31 22:18:07 +0200

    fix cron environement variable

--- start.sh
+++ start.sh
@@ -4,32 +4,34 @@
 if [ ! -d "/minecraft" ]; then
   mkdir /minecraft
 fi
-cd /minecraft
 
 # install dependencies
 apt-get update && apt-get install -y cron nodejs npm jq curl
+cd /app/scheduled-tasks && npm install
+cd /minecraft
 
 # scheduled-tasks
-if [ ! -d "./scheduled-tasks" ]; then
-  if [ -d "/app/scheduled-tasks" ]; then
-    mv /app/scheduled-tasks /minecraft
+if [ ! -f "./tasks.conf.json" ]; then
+  if [ -f "/app/scheduled-tasks/tasks.conf.json" ]; then
+    mv /app/scheduled-tasks/tasks.conf.json /minecraft/tasks.conf.json
   fi
 else
-  if [ -d "/app/scheduled-tasks" ]; then
-    rm -rf /app/scheduled-tasks
+  if [ -f "/app/scheduled-tasks/tasks.conf.json" ]; then
+    rm -rf /app/scheduled-tasks/tasks.conf.json
   fi
 fi
 
-TASKS_FILE="./scheduled-tasks/tasks.conf.json"
+TASKS_FILE="./tasks.conf.json"
 
 crontab -r
+(crontab -l 2>/dev/null; echo "RCON_PASSWORD=${RCON_PASSWORD}") | crontab -
 
 schedules=$(jq -r 'keys[]' $TASKS_FILE)
 
 for schedule in $schedules; do
   cron_time=$(jq -r ".[\"$schedule\"].cron" $TASKS_FILE)
 
-  (crontab -l 2>/dev/null; echo "$cron_time node ./scheduled-tasks/script.js $schedule") | crontab -
+  (crontab -l 2>/dev/null; echo "$cron_time node /app/scheduled-tasks/script.js $schedule") | crontab -
 done
 
 cron 
@@ -40,7 +42,6 @@ FABRIC_SERVER_MC_FILE="fabric-server-mc.${MINECRAFT_VERSION}-loader.${FABRIC_LOA
 if [ ! -f "/minecraft/$FABRIC_SERVER_MC_FILE" ]; then
   rm -rf /minecraft/fabric-server-mc.*
   curl -sS -o $FABRIC_SERVER_MC_FILE https://meta.fabricmc.net/v2/versions/loader/${MINECRAFT_VERSION}/${FABRIC_LOADER_VERSION}/${FABRIC_INSTALLER_VERSION}/server/jar
-  mv $FABRIC_SERVER_MC_FILE /minecraft
   chmod +x /minecraft/$FABRIC_SERVER_MC_FILE
 fi
 
@@ -48,8 +49,6 @@ echo "eula=${EULA}" > eula.txt
 echo "enable-rcon=${RCON_ENABLE}" > server.properties
 echo "rcon.password=${RCON_PASSWORD}" >> server.properties
 echo "rcon.port=25575" >> server.properties
-
-npm install -g rcon
 
 # start
 java -Xms${INIT_MEMORY} -Xmx${MAX_MEMORY} -jar $FABRIC_SERVER_MC_FILE nogui
